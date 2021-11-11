a0a5e568dcf408783c6051b2b7962831a093216e
Vombit <45901675+Vombit@users.noreply.github.com>
2021-11-11 14:54:25 +0500

    Add files via upload

--- client/js/index.js
+++ client/js/index.js
@@ -1,21 +1,28 @@
 let base;
+// Объявление вед сокета
 const myWs = new WebSocket('ws://26.255.29.65:9000');
-// обработчик проинформирует в консоль когда соединение установится
+
+// действие при подключении к сокету
 myWs.onopen = function () {
   console.log('подключено');
   GETDATA();
 };
+
 // обработчик сообщений от сервера
 myWs.onmessage = function (message) {
   base = JSON.parse(message.data)
- // console.log(base);
 
+ maindata()
 };
-// get json data
-function GETDATA() {
-  myWs.send(JSON.stringify({action: 'GetDATA'}));
-}
 
+// Отрисовка данных
+  // Получение данных для главной страницы
+function maindata() {
+  X1();
+  X2();
+  document.getElementsByClassName('X1checkbox')[0].click();
+  document.getElementsByClassName('X2checkbox')[0].click();
+};
 function X1() {
   new Vue({
     el: "#power_monitoringX1",
@@ -32,19 +39,45 @@ function X2() {
     }
   });
 }
-
-  var seee = new Vue({
-  el: '#testtt',
+var X1checkbox = new Vue({
+el: '#X1checkbox',
+data: {
+},
+methods:{
+  X1checkbox(){
+    var keys = Object.keys(this.$refs);
+    var i = 0;
+    while (i < keys.length) {
+      if (base.X1[this.$refs[keys[i]].id]) document.getElementById(this.$refs[keys[i]].id).checked = true;
+      i++;
+    }
+  }
+}
+});
+var X2checkbox = new Vue({
+  el: '#X2checkbox',
   data: {
   },
   methods:{
-    getNameId(){
+    X2checkbox(){
       var keys = Object.keys(this.$refs);
       var i = 0;
       while (i < keys.length) {
-        if (base.X1[this.$refs[keys[i]].id]) document.getElementById(this.$refs[keys[i]].id).checked = true;
+        if (base.X2[this.$refs[keys[i]].id]) document.getElementById(this.$refs[keys[i]].id).checked = true;
         i++;
       }
     }
   }
   });
+// /Отрисовка данных
+
+// Запросы на сервер
+  // Получение данных для главной страницы
+function GETDATA() {
+  myWs.send(JSON.stringify({action: 'GetDATA'}));
+}
+  // Получение данных для НАСТРОЕК
+function GETSETTING() {
+  myWs.send(JSON.stringify({action: 'GetSettings'}));
+}
+// /Запросы на сервер
